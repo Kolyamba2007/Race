909cb29d41a1b6300a4c7f6ccf888a4943d07cc8
Kolyamba2007 <krupenko.ilya2000@gmail.com>
2021-10-23 14:17:26 +0300

    Countdown

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private CarController carController;
+
+    private void Awake()
+    {
+        carController.enabled = false;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+
+    private IEnumerator Countdown()
+    {
+        for (int i = 5; i > 0; i--)
+        {
+            UIManager.CountDown.text = i.ToString();
+            yield return new WaitForSeconds(1);
+        }
+
+        UIManager.CountDown.text = "Start!";
+        carController.enabled = true;
+        yield return new WaitForSeconds(1);
+        UIManager.CountDown.enabled = false;
+
+        yield break;
+    }
+}
