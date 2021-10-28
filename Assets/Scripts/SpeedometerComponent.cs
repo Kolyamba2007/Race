4b6d965841eb01986b323a59fc98b92aceb35a9e
Denis Kozarezov <52127090+DenisKozarezov@users.noreply.github.com>
2021-10-28 21:26:32 +0300

    Added Speedometer

---
+++ Assets/Scripts/SpeedometerComponent.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SpeedometerComponent : MonoBehaviour
+{
+    [SerializeField]
+    private RCC_CarControllerV3 _player;
+    [SerializeField]
+    private TMP_Text _speedText;
+    
+    private void Update()
+    {
+        if (_player.speed == 0) return;
+
+        _speedText.text = $"{Mathf.RoundToInt(_player.speed)} km/h";
+    }
+}
