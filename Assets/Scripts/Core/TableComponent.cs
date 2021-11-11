149e4dd98df46ad0d3bb624e5217077113613125
Kolyamba2007 <krupenko.ilya2000@gmail.com>
2021-11-12 00:46:05 +0300

    Fixes

--- Assets/Scripts/Core/TableComponent.cs
+++ Assets/Scripts/Core/TableComponent.cs
@@ -6,20 +6,15 @@ public class TableComponent : MonoBehaviour
 {
     List<float> _table = new List<float>(10);
 
-    private string ParseTimeToString(float seconds)
-    {
-        return (seconds % 600) < 100 ? $"{seconds / 600}:0{seconds / 10 % 60}:{seconds % 10}" : $"{seconds / 600}:{seconds / 10 % 60}:{seconds % 10}";
-    }
-
-    public void AddItem(float seconds)
+    public void AddItem(string timeText)
     {
         var time = gameObject.transform.GetChild(_table.Count + 1).Find("Time").GetComponent<TMP_Text>();
-        time.text = ParseTimeToString(seconds);
+        time.text = timeText;
     }
-    public void AddItem(int index, float seconds)
+    public void AddItem(int index, string timeText)
     {
         var time = gameObject.transform.GetChild(index + 1).Find("Time").GetComponent<TMP_Text>();
-        time.text = ParseTimeToString(seconds);
+        time.text = timeText;
     }
     public void Clear()
     {
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -31,7 +31,7 @@ public class GameManager : MonoBehaviour
         rccController.enabled = false;
         StopAllCoroutines();
 
-        _tableComponent.AddItem(TimerTime);
+        _tableComponent.AddItem(ParseTimeToString(TimerTime));
         StartCoroutine(TimeDilation());
     }
 
@@ -40,11 +40,17 @@ public class GameManager : MonoBehaviour
         while (true)
         {
             TimerTime++;
-            ChangedTimerTime?.Invoke((TimerTime % 600) < 100 ? $"{TimerTime / 600}:0{TimerTime / 10 % 60}:{TimerTime % 10}" : $"{TimerTime / 600}:{TimerTime / 10 % 60}:{TimerTime % 10}");
+            ChangedTimerTime?.Invoke(ParseTimeToString(TimerTime));
           
             yield return new WaitForSeconds(.1f);
         }
     }
+
+    private string ParseTimeToString(int time)
+    {
+        return (time % 600) < 100 ? $"{time / 600}:0{time / 10 % 60}:{time % 10}" : $"{time / 600}:{time / 10 % 60}:{time % 10}";
+    }
+
     private IEnumerator Countdown()
     {
         for (int i = 5; i > 0; i--)
